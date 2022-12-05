fee2e6cd10fd422d84f7ebdf5fb3fb7eadc2483b
Wen-Binghui <wbhwenbinghui@gmail.com>
2022-12-05 22:43:05 +0100

    1

--- include/deltaCompute/deltaCompute.cpp
+++ include/deltaCompute/deltaCompute.cpp
@@ -7,30 +7,30 @@
 namespace DSONL
 {
 
-    double dot(const Eigen::Vector3d x, const Eigen::Vector3d y)
+    float dot(const Eigen::Vector3f x, const Eigen::Vector3f y)
     {
-        double ans = x.dot(y);
+        float ans = x.dot(y);
         return ans;
     }
 
-    double mod(const double numer, const double denom)
+    float mod(const float numer, const float denom)
     {
-        return fmod(numer, denom);
+        return std::fmod(numer, denom);
     }
 
-    double clamp(const double x, const double min_v, const double max_v)
+    float clamp(const float x, const float min_v, const float max_v)
     {
         return std::min(std::max(x, min_v), max_v);
     }
 
-    double pow(const double x, const double y)
+    float pow(const float x, const float y)
     {
         return std::pow(x, y);
     }
 
-    Eigen::Vector3d pow(const double x, const Eigen::Vector3d y_vec)
+    Eigen::Vector3f pow(const float x, const Eigen::Vector3f y_vec)
     {
-        Eigen::Vector3d out_vec;
+        Eigen::Vector3f out_vec;
         for (int i = 0; i < 3; i++)
         {
             out_vec[i] = pow(x, y_vec[i]);
@@ -38,14 +38,14 @@ namespace DSONL
         return out_vec;
     }
 
-    Eigen::Vector3d pow(const Eigen::Vector3d x, const double y)
+    Eigen::Vector3f pow(const Eigen::Vector3f x, const float y)
     {
         return pow(y, x);
     }
 
-    Eigen::Vector3d pow(const Eigen::Vector3d x_vec, const Eigen::Vector3d y_vec)
+    Eigen::Vector3f pow(const Eigen::Vector3f x_vec, const Eigen::Vector3f y_vec)
     {
-        Eigen::Vector3d out_vec;
+        Eigen::Vector3f out_vec;
         for (int i = 0; i < 3; i++)
         {
             out_vec[i] = pow(x_vec[i], y_vec[i]);
@@ -53,10 +53,10 @@ namespace DSONL
         return out_vec;
     }
 
-    Eigen::Vector3d normalize(const Eigen::Vector3d vec)
+    Eigen::Vector3f normalize(const Eigen::Vector3f vec)
     {
-        Eigen::Vector3d out_vec(vec);
-        double sum_quare(0);
+        Eigen::Vector3f out_vec(vec);
+        float sum_quare(0);
         for (int i = 0; i < 3; i++)
         {
             sum_quare += vec[i] * vec[i];
@@ -74,15 +74,15 @@ namespace DSONL
         return out_vec;
     }
 
-    double mix(const double x, const double y, const double a)
+    float mix(const float x, const float y, const float a)
     {
         if (a > 1 || a < 0)
             throw std::invalid_argument("received value a not in interval(0,1)");
         return x * (1 - a) + y * a;
     }
-    Eigen::Vector3d mix(const Eigen::Vector3d x_vec, const Eigen::Vector3d y_vec, const double a)
+    Eigen::Vector3f mix(const Eigen::Vector3f x_vec, const Eigen::Vector3f y_vec, const float a)
     {
-        Eigen::Vector3d out_vec;
+        Eigen::Vector3f out_vec;
         for (int i = 0; i < 3; i++)
         {
             out_vec[i] = mix(x_vec[i], y_vec[i], a);
@@ -95,24 +95,28 @@ namespace DSONL
         I: Specifies the incident vector.
         N: Specifies the normal vector.
     */
-    Eigen::Vector3d reflect(const Eigen::Vector3d I, const Eigen::Vector3d N)
+    Eigen::Vector3f reflect(const Eigen::Vector3f I, const Eigen::Vector3f N)
     {
-        Eigen::Vector3d N_norm = normalize(N);
-        Eigen::Vector3d out_vec = I - 2 * dot(N_norm, I) * N_norm;
+        Eigen::Vector3f N_norm = normalize(N);
+        Eigen::Vector3f out_vec = I - 2 * dot(N_norm, I) * N_norm;
         return out_vec;
     }
+
     IBL_Radiance::IBL_Radiance() {}
     IBL_Radiance::~IBL_Radiance() {}
-    Vec2f IBL_Radiance::directionToSphericalEnvmap(Vec3f dir) {
-      return cv::Vec2f();
+    Vec2f IBL_Radiance::directionToSphericalEnvmap(Vec3f dir)
+    {
+        return cv::Vec2f();
     }
     Vec3f IBL_Radiance::specularIBL(Vec3f F0, float roughness, Vec3f N,
-                                    Vec3f V) {
-      return cv::Vec3f();
+                                    Vec3f V)
+    {
+        return cv::Vec3f();
     }
     Vec3f IBL_Radiance::diffuseIBL(Vec3f normal) { return cv::Vec3f(); }
-    Vec3f IBL_Radiance::fresnelSchlick(float cosTheta, Vec3f F0) {
-      return cv::Vec3f();
+    Vec3f IBL_Radiance::fresnelSchlick(float cosTheta, Vec3f F0)
+    {
+        return cv::Vec3f();
     }
 
-    }
+}
--- src/test_functions.cpp
+++ src/test_functions.cpp
@@ -2,37 +2,30 @@
 //#include "thread"
 int main()
 {
-    Eigen::Vector3d x;
-    Eigen::Vector3d y;
-    x << 1, 1, -1;
-    y << 0, 0, 1;
-    double c = DSONL::dot(x, y);
-    printf("dot = %f\n", c);
-    printf("mod = %f\n", DSONL::mod(10.002, 1.0));
-    printf("clamp = %f\n", DSONL::clamp(10.002, 1.0, 3));
-    printf("clamp = %f\n", DSONL::clamp(0.002, 1.111, 3));
-    // std::cout << DSONL::pow(0.002, x) << std::endl;
-    // std::cout << DSONL::pow(y, y) << std::endl;
+  Eigen::Vector3f x;
+  Eigen::Vector3f y;
+  x << 1, 1, -1;
+  y << 0, 0, 1;
+  double c = DSONL::dot(x, y);
+  printf("dot = %f\n", c);
+  printf("mod = %f\n", DSONL::mod(10.002, 1.0));
+  printf("clamp = %f\n", DSONL::clamp(10.002, 1.0, 3));
+  printf("clamp = %f\n", DSONL::clamp(0.002, 1.111, 3));
+  // std::cout << DSONL::pow(0.002, x) << std::endl;
+  // std::cout << DSONL::pow(y, y) << std::endl;
 
-    std::cout << DSONL::normalize(y) << std::endl
-              << std::endl;
-    std::cout << DSONL::mix(x, y, 0.1) << std::endl
-              << std::endl;
+  std::cout << DSONL::normalize(y) << std::endl
+            << std::endl;
+  std::cout << DSONL::mix(x, y, 0.1) << std::endl
+            << std::endl;
 
-    std::cout << DSONL::reflect(x, y) << std::endl
-              << std::endl;
-    return 0;
+  std::cout << DSONL::reflect(x, y) << std::endl
+            << std::endl;
+  return 0;
 
-      // Bro Binghui: Hi! ヽ(✿ﾟ▽ﾟ)ノ, please have your todos here~
+  // Bro Binghui: Hi! ヽ(✿ﾟ▽ﾟ)ノ, please have your todos here~
 
-      //TODO: 1. template functions
-      //      2. parallel for-loops ( double index-based for-loop), and multi-threads
-      //      3. locate the position in DSO(or DSM...) where we plug our IBL radiance code
-
-
-
-
-
-
-
+  //TODO: 1. template functions
+  //      2. parallel for-loops ( double index-based for-loop), and multi-threads
+  //      3. locate the position in DSO(or DSM...) where we plug our IBL radiance code
 }
